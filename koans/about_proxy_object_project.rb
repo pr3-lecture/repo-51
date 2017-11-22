2d35163ad4ab73ae7fdbbf425ab736908a193d8f
Dennis <d.czogalla@gmx.de>
2017-11-22 10:35:21 +0100

    282/282

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -13,6 +13,8 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages
+
   def initialize(target_object)
     @object = target_object
     @messages = []
