16aefb95630577b198d1a92d6caff4dd1bce6174
Dennis <d.czogalla@gmx.de>
2017-11-27 14:33:47 +0100

    update

--- lisp_uebung/LispUebungsblatt1.lisp
+++ lisp_uebung/LispUebungsblatt1.lisp
@@ -1,74 +1,75 @@
-Aufgabe 1:
+; Aufgabe 1:
 
-a)
+; a)
 (defun rotiere (x) (append (rest x) (list (first x))))
 
-b)
+; b)
 (defun neues-vorletztes (param1 param2)
   (append
     (append
       (loop for x from 1 to (- (list-length param2) 1)
         collect (nth x param)) param1) (last param2)))
 
-c)
+; c)
 (defun my-length (param) (loop for x in param count x))
 
-ODER
+; ODER
 
 (defun my-length (param) (list-length param))
 
-d)
+; d)
 (defun my-lengthR (param)(loop for x in param sum
   (if (listp x)
     (my-lengthR x)
     1)))
 
-e)
+; e)
 (defun my-reverse (param)(loop for x from 1 to (list-length param)
   collect (
     nth (- (list-length param) x) param
   )))
 
-f)
+; f)
 (defun my-reverseR (param)(loop for x from 1 to (list-length param)
   collect (if (listp (setq item (nth (- (list-length param) x) param)))
      (my-reverseR item)
       item
       )))
 
-Aufgabe 2:
+; Aufgabe 2:
 
-https://lvb-wissen.de/informatik/datenstrukturen/binearbaum/traversierungsverfahren:-Preorder-Inorder-und-Postorder-8592095901.html
+; https://lvb-wissen.de/informatik/datenstrukturen/binearbaum/traversierungsverfahren:-Preorder-Inorder-und-Postorder-8592095901.html
 
-a)
+; a)
 (10 (5 (3) (7)) (13 (12) (20)))
 
-    10
-    / \
-   5   13
-  /\   /\
- 3  7 12 20
+;    10
+;    / \
+;   5   13
+;  /\   /\
+; 3  7 12 20
 
-b)
-PREORDER:
+; b)
+
+; PREORDER:
 (defun preorder(param) (print (first param))
   (if (nth 1 param)(preorder (nth 1 param)))
   (if (nth 2 param)(preorder (nth 2 param))))
 
-Ausgabe: 10 5 3 7 13 12 20
+; Ausgabe: 10 5 3 7 13 12 20
 
-INORDER:
+; INORDER:
 (defun inorder(param)
   (if (nth 1 param)(inorder (nth 1 param)))
   (print (first param))
   (if (nth 2 param)(inorder (nth 2 param))))
 
-Ausgabe: 3 5 7 10 12 13 20
+; Ausgabe: 3 5 7 10 12 13 20
 
-POSTORDER:
+; POSTORDER:
 (defun postorder(param)
   (if (nth 1 param)(postorder (nth 1 param)))
   (if (nth 2 param)(postorder (nth 2 param)))
   (print (first param)))
 
-Ausgabe:  3 7 5 12 20 13 10
+; Ausgabe:  3 7 5 12 20 13 10
